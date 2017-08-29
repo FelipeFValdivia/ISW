7bdf155e0af4b754f59926d18af3fe9abc9827ce
Felipe Flores <fflores@nursoft.cl>
2017-08-28 23:39:23 -0300

    fix problem with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  before_create :set_first_birth_date
   has_many :user_courses
   has_many :courses, through: :user_courses
 
